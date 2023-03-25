66477609c52723964b7b1a8daed0234db42e2d9e
Jonathan Lee <60332142+jonathanlee04@users.noreply.github.com>
2023-03-25 23:02:14 +0800

    Add files via upload

---
+++ Reiz_assign/Reiz_Assign1/Reiz_Assign1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReizAssign1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour, min;
+            double hourAngle, minAngle, angle;
+
+            Console.WriteLine("Enter hours from 0 to 12:"); //User enter hours 0 to 12
+            hour = int.Parse(Console.ReadLine()); //Read input data and set to variable hour
+
+            Console.WriteLine("Enter minutes from 0 to 59:"); //User enter minute 0 ot 59
+            min = int.Parse(Console.ReadLine()); //Read input data and set to vairbale min
+
+            //given circle = 360
+            //360 degrees / 12 hours = 30 degrees per hour
+            //360 degrees / 60 minutes = 6 degrees per minute
+
+            hourAngle = (hour % 12 + (double)min / 60) * 30; //30 degrees per hour
+            minAngle = min * 6; //6 degrees per minute
+
+            angle = Math.Abs(hourAngle - minAngle);
+            if (angle > 180) //getting the smaller angle
+            {
+                angle = 360 - angle;
+            }
+
+            Console.WriteLine($"\nThe angle between the hour and minute hands is {angle} degrees.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Reiz_assign/Reiz_Assign2/Reiz_Assign2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Branch
+{
+    public List<Branch> branches = new List<Branch>();
+}
+
+class Program
+{
+    static int CalculateDepth(Branch branch)
+    {
+        if (branch.branches.Count == 0) //set counter
+        {
+            return 1;
+        }
+        else
+        {
+            int maxDepth = 0; //initialize variable for maximun depth
+            foreach (Branch subBranch in branch.branches)
+            {
+                int subDepth = CalculateDepth(subBranch); //recursive method
+                if (subDepth > maxDepth)
+                {
+                    maxDepth = subDepth;
+                }
+            }
+            return maxDepth + 1;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Branch root = new Branch();
+        root.branches.Add(new Branch()); //depth 1 with 2 branches
+        root.branches.Add(new Branch()); 
+        root.branches[0].branches.Add(new Branch()); //root.branch1 with 1 branch
+        root.branches[1].branches.Add(new Branch()); //root.branch2 with 3 branches
+        root.branches[1].branches.Add(new Branch());
+        root.branches[1].branches.Add(new Branch());
+        root.branches[1].branches[0].branches.Add(new Branch()); //root.branch2.branch1 with 1 branch
+        root.branches[1].branches[1].branches.Add(new Branch()); //root.branch2.branch2 with 2 branches
+        root.branches[1].branches[1].branches.Add(new Branch());
+        root.branches[1].branches[1].branches[0].branches.Add(new Branch()); //root.branch2.branch2.branch1 with 1 branch
+
+        //the expected output of depth should be 5
+
+        int depth = CalculateDepth(root); //a recursive method that calculates the depth of a given branch
+        Console.WriteLine("Depth: " + depth);
+
+        Console.ReadKey();
+    }
+}
